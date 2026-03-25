52d6010aad2585f565ed9e69a0b98863e1ee5dc7
huteng <huteng@echo.tech>
2026-03-25 15:07:10 +0800

    feat: add missing bridge

---
+++ fe/packages/service/src/api/core/audio/index.js
@@ -0,0 +1,15 @@
+
+
+import { invokeAPI } from '@/api/common'
+
+/**
+ * 发起 HTTPS 网络请求
+ * https://developers.weixin.qq.com/miniprogram/en/dev/api/media/audio/wx.createInnerAudioContext.html
+ * @param {*} opts
+ */
+export function createInnerAudioContext() {
+    // HT: temp fake
+    return {}
+
+	// invokeAPI('createInnerAudioContext', opts)
+}
---
+++ fe/packages/service/src/api/core/filesystem/index.js
@@ -0,0 +1,13 @@
+import { invokeAPI } from '@/api/common'
+
+/**
+ * 文件管理器
+ * https://developers.weixin.qq.com/miniprogram/dev/api/file/wx.getFileSystemManager.html
+ * @param {*} opts
+ */
+export function getFileSystemManager() {
+    // HT: temp fake
+    return {}
+
+	// invokeAPI('getFileSystemManager', opts)
+}
